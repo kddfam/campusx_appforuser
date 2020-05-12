fa8ec5caf5b8af79f9d26481fa40e61135e52bd6
developer <kdd.family.003@gmail.com>
2020-05-12 08:05:08 +0530

    v1.0

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: HomeScreen()
+));
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
