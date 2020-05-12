5defd8a04bf6abe392e9ca6e3084909d64203bb8
developer <kdd.family.003@gmail.com>
2020-05-12 16:06:14 +0530

    v1.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
+import 'package:campusx/screen/shoplist.dart';
+import 'package:campusx/screen/shoplistdetailed.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: HomeScreen()
+  home: HomeScreen(),
+  initialRoute: '/',
+  routes: {
+    ShopList.routeName : (context) => ShopList(),
+    ShopListDetailed.routeName : (context) => ShopListDetailed()
+  },
 ));
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, ShopList.routeName);
+            },
+            child: Text('Move to Next'),
+          ),
+        ),
+      ),
+    );
   }
 }
 
